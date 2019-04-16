d4723532f2e6f6993d64b576e3a6af17d7894e2b
Ubuntu <ubuntu@ip-172-31-21-153.us-east-2.compute.internal>
2019-04-16 03:53:05 +0000

    Adding new files

---
+++ strcon.sh
@@ -0,0 +1,7 @@
+string1="Ragul Ravi"
+string2=" Raja"
+echo "$string1$string2"
+string3=$string1+$string2
+string3+=" works in HCL"
+echo $string3
+
---
+++ substr.sh
@@ -0,0 +1,4 @@
+Str="Learn Linux from LinuxHint"
+subStr=${Str:17:9}
+echo $subStr
+
